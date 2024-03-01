5a259b8ca8493ebff9444ccaa4449af4a8c34bf3
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-03-01 17:53:49 +0100

    rollback transaction

--- table.js
+++ table.js
@@ -1,5 +1,12 @@
 // You decide to remove a book with BookID = 101 from the Books table. Ensure this change is saved permanently.
 
+// BEGIN TRANSACTION;
+// DELETE FROM Book WHERE id = 101;
+// COMMIT;
+
+
+// You're testing the database by removing a book with BookID = 103 but want to revert the change immediately after.
+
 BEGIN TRANSACTION;
-DELETE FROM Book WHERE id = 101;
-COMMIT;
+DELETE FROM Book WHERE id = 103;
+ROLLBACK;
